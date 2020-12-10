3ef437e5dcb1d3e77bec05a8782006bfb83a39c9
Varsha Hulikantimutt <VarshaHulikantimutt@users.noreply.github.com>
2020-12-10 18:36:59 +0530

    Final commit

--- translate.js
+++ translate.js
@@ -1,7 +1,30 @@
 var translateButton = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#text-input");
+var outputText = document.querySelector("#text-output");
 
-translateButton.addEventListener("click",clickHandler);
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("There is an Error ", error);
+    alert("Something went wrong! Please try again after some time.")
+}
 
 function clickHandler(){
-    console.log("Clicked");
+    var text = inputText.value; 
+
+
+    fetch(getTranslationURL(text))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputText.innerText = translatedText;
+           })
+        .catch(errorHandler)
 }
+
+translateButton.addEventListener("click",clickHandler);
